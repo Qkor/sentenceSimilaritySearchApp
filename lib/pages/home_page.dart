aac844077e17f29e2f7ac2792d9d5a9dd205b24f
jk <jan.kukorowski@wp.pl>
2024-08-23 15:34:47 +0200

    ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,27 +21,63 @@ class _HomePageState extends State<HomePage> {
             child: Column(
               children: [
                 const SizedBox(height: 30),
-                TextField(
-                  controller: inputController,
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Stack(
+                    children: [
+                      TextField(
+                        controller: inputController,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: IconButton(
+                            onPressed: (){
+                              final results = VectorDatabaseManager.query(inputController.text);
+                              setState(() {
+                                answers = [];
+                                for(final result in results){
+                                  answers.add(result.object.text);
+                                }
+                              });
+                            },
+                            icon: const Icon(Icons.search)
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-                const SizedBox(height: 30),
-                ElevatedButton(
-                  onPressed: (){
-                    final results = VectorDatabaseManager.query(inputController.text);
-                    setState(() {
-                      answers = [];
-                      for(final result in results){
-                        answers.add(result.object.text);
-                      }
-                    });
-                  },
-                  child: const Text("Search")
-                ),
-                ElevatedButton(
-                    onPressed: () async {
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10,40,10,10),
+                  child: TextButton(
+                      onPressed: () async {
                         String? text = await FileManager.loadTextFile();
+                        if(text != null){
+                          VectorDatabaseManager.chunkAndPut(text);
+                        }
+                        if(context.mounted){
+                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("File content loaded"), backgroundColor: Colors.green));
+                        }
+                      },
+                      child: const Row(children: [
+                        Icon(Icons.file_open),
+                        Text('Load vector database from txt file')
+                      ])
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextButton(
+                    onPressed: () {
+                      VectorDatabaseManager.clearDatabase();
+                      if(context.mounted){
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Database cleared"), backgroundColor: Colors.green));
+                      };
                     },
-                    child: const Text("Select file")
+                      child: const Row(children: [
+                        Icon(Icons.clear_outlined),
+                        Text('Clear the database')
+                      ])
+                  ),
                 ),
                 const SizedBox(height: 30),
                 for(final answer in answers) ... {
